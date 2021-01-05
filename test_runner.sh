6ebb8eecbfc823c0ed8c4da0662965ea7f39d7d4
Yasasr1 <2017is070@stu.ucsc.cmb.ac.lk>
2021-01-05 10:53:43 +0530

    test runner script updated

--- test_runner.sh
+++ test_runner.sh
@@ -9,7 +9,7 @@ echo
 echo "Basic certification test plan"
 echo "-----------------------------"
 echo
-if sudo python3 ../conformance-suite/scripts/run-test-plan.py oidcc-basic-certification-test-plan[server_metadata=static][client_registration=static_client] ./IS_config.json 2>&1 | tee basic-certification-test-plan-log.txt
+if sudo python3 ./conformance-suite/scripts/run-test-plan.py oidcc-basic-certification-test-plan[server_metadata=static][client_registration=static_client] ./IS_config.json 2>&1 | tee basic-certification-test-plan-log.txt
 then
 	IS_FAILED=true
     failed_test_plans+=("Basic certification test plan")
@@ -18,7 +18,7 @@ echo
 echo "Implicit certification test plan"
 echo "-----------------------------"
 echo
-if sudo python3 ../conformance-suite/scripts/run-test-plan.py oidcc-implicit-certification-test-plan[server_metadata=static][client_registration=static_client] ./IS_config.json 2>&1 | tee implicit-certification-test-plan-log.txt
+if sudo python3 ./conformance-suite/scripts/run-test-plan.py oidcc-implicit-certification-test-plan[server_metadata=static][client_registration=static_client] ./IS_config.json 2>&1 | tee implicit-certification-test-plan-log.txt
 then
 	IS_FAILED=true
     failed_test_plans+=("Implicit certification test plan")
@@ -27,7 +27,7 @@ echo
 echo "Hybrid certification test plan"
 echo "-----------------------------"
 echo
-if sudo python3 ../conformance-suite/scripts/run-test-plan.py oidcc-hybrid-certification-test-plan[server_metadata=static][client_registration=static_client] ./IS_config.json 2>&1 | tee hybrid-certification-test-plan-log.txt
+if sudo python3 ./conformance-suite/scripts/run-test-plan.py oidcc-hybrid-certification-test-plan[server_metadata=static][client_registration=static_client] ./IS_config.json 2>&1 | tee hybrid-certification-test-plan-log.txt
 then
 
 	IS_FAILED=true
@@ -37,7 +37,7 @@ echo
 echo "Formpost basic certification test plan"
 echo "-----------------------------"
 echo
-if sudo python3 ../conformance-suite/scripts/run-test-plan.py oidcc-formpost-basic-certification-test-plan[server_metadata=static][client_registration=static_client] ./IS_config.json 2>&1 | tee formpost-basic-certification-test-plan-log.txt
+if sudo python3 ./conformance-suite/scripts/run-test-plan.py oidcc-formpost-basic-certification-test-plan[server_metadata=static][client_registration=static_client] ./IS_config.json 2>&1 | tee formpost-basic-certification-test-plan-log.txt
 then
 
 	IS_FAILED=true
@@ -47,7 +47,7 @@ echo
 echo "Formpost implicit certification test plan"
 echo "-----------------------------"
 echo
-if sudo python3 ../conformance-suite/scripts/run-test-plan.py oidcc-formpost-implicit-certification-test-plan[server_metadata=static][client_registration=static_client] ./IS_config.json 2>&1 | tee formpost-implicit-certification-test-plan-log.txt
+if sudo python3 ./conformance-suite/scripts/run-test-plan.py oidcc-formpost-implicit-certification-test-plan[server_metadata=static][client_registration=static_client] ./IS_config.json 2>&1 | tee formpost-implicit-certification-test-plan-log.txt
 then
 
 	IS_FAILED=true
@@ -57,7 +57,7 @@ echo
 echo "Formpost hybrid certification test plan"
 echo "-----------------------------"
 echo
-if sudo python3 ../conformance-suite/scripts/run-test-plan.py oidcc-formpost-hybrid-certification-test-plan[server_metadata=static][client_registration=static_client] ./IS_config.json 2>&1 | tee formpost-hybrid-certification-test-plan-log.txt
+if sudo python3 ./conformance-suite/scripts/run-test-plan.py oidcc-formpost-hybrid-certification-test-plan[server_metadata=static][client_registration=static_client] ./IS_config.json 2>&1 | tee formpost-hybrid-certification-test-plan-log.txt
 then
 
 	IS_FAILED=true
@@ -67,7 +67,7 @@ echo
 echo "Exporting results"
 echo "-----------------------------"
 echo
-sudo python3 ./export_results.py $CONFORMANCE_SUITE_URL
+sudo python3 ./wso2-identity-server-automated-config/export_results.py $CONFORMANCE_SUITE_URL
 
 if $IS_FAILED
 then
