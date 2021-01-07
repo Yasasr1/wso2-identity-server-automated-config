4bf14e23b93ff69afc4f62d6d5366c54f7041456
Yasasr1 <2017is070@stu.ucsc.cmb.ac.lk>
2021-01-07 13:24:19 +0530

    updated

--- export_results.py
+++ export_results.py
@@ -26,13 +26,13 @@ def get_failed_tests(plan):
 failed_plan_details = dict()
 warnings.filterwarnings("ignore")
 plan_list = json.loads(requests.get(url=sys.argv[1] + "/api/plan?length=15", verify=False).content)
-
+print(plan_list)
 for test_plan in plan_list['data']:
     # save_results(test_plan)
     failed_tests_list = get_failed_tests(test_plan)
     if len(failed_tests_list) > 0:
         failed_plan_details[test_plan['planName']] = failed_tests_list
-print(failed_plan_details)
+#print(failed_plan_details)
 if failed_plan_details:
     print("Following tests failed\n===========================")
     for test_plan in failed_plan_details:
--- test_runner.sh
+++ test_runner.sh
@@ -8,12 +8,12 @@ echo
 echo "Basic certification test plan"
 echo "-----------------------------"
 echo
-sudo python3 ./conformance-suite/scripts/run-test-plan.py oidcc-basic-certification-test-plan[server_metadata=static][client_registration=static_client] ./IS_config.json 2>&1 | tee basic-certification-test-plan-log.txt
-echo
-echo "Implicit certification test plan"
-echo "-----------------------------"
-echo
-sudo python3 ./conformance-suite/scripts/run-test-plan.py oidcc-implicit-certification-test-plan[server_metadata=static][client_registration=static_client] ./IS_config.json 2>&1 | tee implicit-certification-test-plan-log.txt
+#sudo python3 ./conformance-suite/scripts/run-test-plan.py oidcc-basic-certification-test-plan[server_metadata=static][client_registration=static_client] ./IS_config.json 2>&1 | tee basic-certification-test-plan-log.txt
+#echo
+#echo "Implicit certification test plan"
+#echo "-----------------------------"
+#echo
+#sudo python3 ./conformance-suite/scripts/run-test-plan.py oidcc-implicit-certification-test-plan[server_metadata=static][client_registration=static_client] ./IS_config.json 2>&1 | tee implicit-certification-test-plan-log.txt
 #echo
 #echo "Hybrid certification test plan"
 #echo "-----------------------------"
@@ -35,7 +35,7 @@ sudo python3 ./conformance-suite/scripts/run-test-plan.py oidcc-implicit-certifi
 #echo
 #sudo python3 ./conformance-suite/scripts/run-test-plan.py oidcc-formpost-hybrid-certification-test-plan[server_metadata=static][client_registration=static_client] ./IS_config.json 2>&1 | tee formpost-hybrid-certification-test-plan-log.txt
 echo
-if sudo python3 ./wso2-identity-server-automated-config/export_results.py $CONFORMANCE_SUITE_URL
+if sudo python3 ./export_results.py $CONFORMANCE_SUITE_URL
 then
   IS_FAILED=true
 fi
