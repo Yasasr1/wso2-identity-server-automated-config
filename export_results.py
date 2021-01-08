9923571e7df852f8182b603dd6f3a34d4318eb67
Yasasr1 <2017is070@stu.ucsc.cmb.ac.lk>
2021-01-08 13:13:10 +0530

    updated

--- export_results.py
+++ export_results.py
@@ -29,14 +29,18 @@ def get_failed_tests(plan):
 
 
 failed_plan_details = dict()
+contains_fails = False
 warnings.filterwarnings("ignore")
 plan_list = json.loads(requests.get(url=sys.argv[1] + "/api/plan?length=50", verify=False).content)
 print("======================\nExporting test results\n======================")
 for test_plan in plan_list['data']:
     save_results(test_plan)
     failed_tests_list = get_failed_tests(test_plan)
+    if len(failed_tests_list['fails']) > 0:
+        contains_fails = True
     if len(failed_tests_list['fails']) > 0 or len(failed_tests_list['warnings']) > 0:
         failed_plan_details[test_plan['planName']] = failed_tests_list
+
 if failed_plan_details:
     print("Following tests have fails/warnings\n===========================")
     for test_plan in failed_plan_details:
@@ -45,8 +49,18 @@ if failed_plan_details:
         print(*failed_plan_details[test_plan]['fails'], sep="\n")
         print("\nWarnings\n--------")
         print(*failed_plan_details[test_plan]['warnings'], sep="\n")
-    sys.exit(1)
+    if contains_fails:
+        sys.exit(1)
+    else:
+        sys.exit(0)
 else:
     print("\nAll test plans finished successfully")
     sys.exit(0)
 
+
+{
+    "test_name": {
+        'fails': [],
+        'warnings': []
+    }
+}
