eda80e3717bb3dcf590e4b14c941727eb501f0b0
Yasasr1 <2017is070@stu.ucsc.cmb.ac.lk>
2021-01-15 09:17:26 +0530

    updated test_runner.sh

--- test_runner.sh
+++ test_runner.sh
@@ -8,32 +8,32 @@ echo
 echo "Basic certification test plan"
 echo "-----------------------------"
 echo
-sudo python3 ./conformance-suite/scripts/run-test-plan.py oidcc-basic-certification-test-plan[server_metadata=static][client_registration=static_client] ./Basic_test_plan/IS_config_basic.json 2>&1 | tee basic-certification-test-plan-log.txt
+sudo python3 ./conformance-suite/scripts/run-test-plan.py oidcc-basic-certification-test-plan[server_metadata=static][client_registration=static_client] ./wso2-identity-server-automated-config/Basic_test_plan/IS_config_basic.json 2>&1 | tee basic-certification-test-plan-log.txt
 echo
 echo "Implicit certification test plan"
 echo "-----------------------------"
 echo
-sudo python3 ./conformance-suite/scripts/run-test-plan.py oidcc-implicit-certification-test-plan[server_metadata=static][client_registration=static_client] ./Implicit_test_plan/IS_config_implicit.json 2>&1 | tee implicit-certification-test-plan-log.txt
+sudo python3 ./conformance-suite/scripts/run-test-plan.py oidcc-implicit-certification-test-plan[server_metadata=static][client_registration=static_client] ./wso2-identity-server-automated-config/Implicit_test_plan/IS_config_implicit.json 2>&1 | tee implicit-certification-test-plan-log.txt
 echo
 echo "Hybrid certification test plan"
 echo "-----------------------------"
 echo
-sudo python3 ./conformance-suite/scripts/run-test-plan.py oidcc-hybrid-certification-test-plan[server_metadata=static][client_registration=static_client] ./Hybrid_test_plan/IS_config_hybrid.json 2>&1 | tee hybrid-certification-test-plan-log.txt
+sudo python3 ./conformance-suite/scripts/run-test-plan.py oidcc-hybrid-certification-test-plan[server_metadata=static][client_registration=static_client] ./wso2-identity-server-automated-config/Hybrid_test_plan/IS_config_hybrid.json 2>&1 | tee hybrid-certification-test-plan-log.txt
 echo
 #echo "Formpost basic certification test plan"
 #echo "-----------------------------"
 #echo
-sudo python3 ./conformance-suite/scripts/run-test-plan.py oidcc-formpost-basic-certification-test-plan[server_metadata=static][client_registration=static_client] ./Formpost_basic_test_plan/IS_config_formpost_basic.json 2>&1 | tee formpost-basic-certification-test-plan-log.txt
+sudo python3 ./conformance-suite/scripts/run-test-plan.py oidcc-formpost-basic-certification-test-plan[server_metadata=static][client_registration=static_client] ./wso2-identity-server-automated-config/Formpost_basic_test_plan/IS_config_formpost_basic.json 2>&1 | tee formpost-basic-certification-test-plan-log.txt
 #echo
 #echo "Formpost implicit certification test plan"
 #echo "-----------------------------"
 #echo
-sudo python3 ./conformance-suite/scripts/run-test-plan.py oidcc-formpost-implicit-certification-test-plan[server_metadata=static][client_registration=static_client] ./Formpost_implicit_test_plan/IS_config_formpost_implicit.json 2>&1 | tee formpost-implicit-certification-test-plan-log.txt
+sudo python3 ./conformance-suite/scripts/run-test-plan.py oidcc-formpost-implicit-certification-test-plan[server_metadata=static][client_registration=static_client] ./wso2-identity-server-automated-config/Formpost_implicit_test_plan/IS_config_formpost_implicit.json 2>&1 | tee formpost-implicit-certification-test-plan-log.txt
 #echo
 #echo "Formpost hybrid certification test plan"
 #echo "-----------------------------"
 #echo
-sudo python3 ./conformance-suite/scripts/run-test-plan.py oidcc-formpost-hybrid-certification-test-plan[server_metadata=static][client_registration=static_client] ./Formpost_hybrid_test_plan/IS_config_formpost_hybrid.json 2>&1 | tee formpost-hybrid-certification-test-plan-log.txt
+sudo python3 ./conformance-suite/scripts/run-test-plan.py oidcc-formpost-hybrid-certification-test-plan[server_metadata=static][client_registration=static_client] ./wso2-identity-server-automated-config/Formpost_hybrid_test_plan/IS_config_formpost_hybrid.json 2>&1 | tee formpost-hybrid-certification-test-plan-log.txt
 echo
 if sudo python3 ./wso2-identity-server-automated-config/export_results.py $CONFORMANCE_SUITE_URL
 then
