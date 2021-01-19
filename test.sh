3d2303c2aca8717aca6776564678af4df0314d2c
Yasasr1 <2017is070@stu.ucsc.cmb.ac.lk>
2021-01-19 11:04:15 +0530

    updated test.sh

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 echo $GITHUB_RUN_NUMBER
-echo ${{job.status}}
-echo ${{github.repository}}
-${{github.repository}}
-${{secrets.GOOGLE_WEBHOOK}}
-echo ${{secrets.GOOGLE_WEBHOOK}}
+echo ${job.status}
+echo ${github.repository}
+${github.repository}
+${secrets.GOOGLE_WEBHOOK}
+echo ${secrets.GOOGLE_WEBHOOK}
